c9de50abc6a1b58819ee9a8eb21390e6f97c7193
DrownedFish74 <drowned.lolita@gmail.com>
2020-05-24 11:04:49 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,16 @@ Rails.application.routes.draw do
   resources :entrance
   resources :user_sessions, only: [:new, :create, :destroy]
   resources :users, only: [:index,:new, :create, :show] do
-    resources :books do
-      resources :impressions
-    end
-    resources :wishes
     resources :bookshelves
-    resources :friends
-    resources :book_searches
-    resources :bookmove, only:[:index, :create]
   end
+  resources :books do
+    resources :impressions
+  end
+  resources :wishes
+  resources :friends
+  resources :book_searches
+  resources :bookmove, only:[:index, :create]
   resources :wishes, only: :show
   resources :bookshelves, only: :show
   root to: "entrance#index"
-  
 end
